20e381e693f336fe7dc277d0b91ba96615bcf545
Pablo Henry <87196825+PabloPikassso@users.noreply.github.com>
2025-08-12 22:20:29 -0300

    Add files via upload
    english

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package git;
+
+import java.util.Scanner;
+
+public class Hello {
+	static Scanner scan= new Scanner(System.in);
+	
+	public static void say() {
+		System.out.println("Hello world");
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		//say();
+		
+	}
+
+}
