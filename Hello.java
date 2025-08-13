7a94dabb8d8e57c74fbee14e6cc1f1370507cbb9
Pablo Henry <87196825+PabloPikassso@users.noreply.github.com>
2025-08-12 23:04:10 -0300

    Update Hello.java
    fix

--- Hello.java
+++ Hello.java
@@ -9,14 +9,13 @@ public class Hello {
 		System.out.println("Hello world");
 	}
 	
-	public static void sayPortuguese() {
-		System.out.println("Ola mundo");
-	}
+	
 	
 	public static void main(String[] args) {
 		
-		sayPortuguese();
+		say();
 		
 	}
 
 }
+
